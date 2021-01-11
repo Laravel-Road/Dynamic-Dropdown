0f6b665069d7ca2949d7446b120e6b7206897a0b
Pedro Augusto <pams.pedro@gmail.com>
2021-01-10 23:38:33 -0300

    Refacto to use localtions facade

--- app/Http/Livewire/DynamicDropdown.php
+++ app/Http/Livewire/DynamicDropdown.php
@@ -2,54 +2,26 @@
 
 namespace App\Http\Livewire;
 
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
+use App\Facades\Locations;
 use Livewire\Component;
 
 class DynamicDropdown extends Component
 {
-    public $estado;
-    public $municipios;
+    public $state;
+    public $cities;
 
-    public function getClientProperty()
+    public function getStatesProperty()
     {
-        return Http::baseUrl('https://servicodados.ibge.gov.br/api/v1/localidades');
+        return Locations::getStates();
     }
 
-    public function getEstadosProperty()
+    public function updatedState()
     {
-        if (! Cache::has( 'estados')) {
-            Cache::put('estados', $this->getEstados(), 3600);
-        }
-
-        return Cache::get('estados');
-    }
-
-    public function updatedEstado()
-    {
-        if (! Cache::has( $this->estado . '_municipios')) {
-            Cache::put($this->estado . '_municipios', $this->getMunicipios(), 3600);
-        }
-
-        $this->municipios = Cache::get($this->estado . '_municipios');
+        $this->cities = Locations::getCities($this->state);
     }
 
     public function render()
     {
         return view('livewire.dynamic-dropdown');
-    }
-
-    private function getEstados()
-    {
-        return $this->client
-            ->get('/estados', ['orderBy' => 'nome'])
-            ->object();
-    }
-
-    private function getMunicipios()
-    {
-        return $this->client
-            ->get("/estados/{$this->estado}/municipios")
-            ->object();
     }
 }
--- resources/views/livewire/dynamic-dropdown.blade.php
+++ resources/views/livewire/dynamic-dropdown.blade.php
@@ -1,10 +1,10 @@
 <div>
     <div class="my-8 mx-4">
-        <label for="estado" class="block text-sm font-medium text-gray-700">Estado</label>
-        <select wire:model="estado" id="estado" name="estado" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-200 focus>outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+        <label for="state" class="block text-sm font-medium text-gray-700">Estado</label>
+        <select wire:model="state" id="state" name="state" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-200 focus>outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
             <option value="" selected>{{ __('Selecione um estado...') }}</option>
-            @foreach($this->estados as $estado)
-                <option value="{{ $estado->sigla }}">{{ $estado->nome }}</option>
+            @foreach($this->states as $state)
+                <option value="{{ $state->initials }}">{{ $state->name }}</option>
             @endforeach
         </select>
     </div>
@@ -13,9 +13,9 @@
         <label for="cidade" class="block text-sm font-medium text-gray-700">Cidade</label>
         <select id="cidade" name="cidade" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-200 focus>outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
             <option value="" selected>{{ __('Selecione uma cidade...') }}</option>
-            @if($this->estado)
-                @foreach($this->municipios as $municipio)
-                    <option value="{{ $municipio->id }}">{{ $municipio->nome }}</option>
+            @if($this->state)
+                @foreach($this->cities as $city)
+                    <option value="{{ $city->id }}">{{ $city->name }}</option>
                 @endforeach
             @endif
         </select>
