5373d6e86c9066ec14b7e3153247f4b6c9dbe36b
Pedro Augusto <pams.pedro@gmail.com>
2021-01-11 02:50:56 -0300

    Remove files on package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Services\LocationsService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(LocationsService::class, function () {
-            return new LocationsService();
-        });
+        //
     }
 
     /**
